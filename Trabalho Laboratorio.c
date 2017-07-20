a48a6ba1addd78fd5af6e5ecb89b95a1046bb5b5
DaniloD30 <danilolive30@hotmail.com>
2017-07-20 18:54:38 -0300

    Update Trabalho Laboratorio.c

--- Trabalho Laboratorio.c
+++ Trabalho Laboratorio.c
@@ -11,10 +11,10 @@
 //  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
 
 //  ----- Dados do Aluno -----
-//  Nome:
+//  Nome: Danilo Oliveira Dias
 //  email:
-//  Matrícula:
-//  Semestre:
+//  Matrícula: 2016116013
+//  Semestre: Terceiro
 
 //  Copyright © 2016 Renato Novais. All rights reserved.
 // Última atualização: 19/08/2016
@@ -25,6 +25,11 @@
 #include <stdlib.h>
 #include<string.h>
 #include <ctype.h>
+#include<locale.h>
+int QtdDigitosBase(int numerobase);
+int QtdDigitosBusca(int numerobusca);
+int x;
+int y;
 /*
  Q1 = validar data
 @objetivo
@@ -37,15 +42,15 @@
  */
 int q1(char *data){
     int datavalida = 1;
-    
+
     //printf("%s\n", data);
-    
-    
+
+
     if (datavalida)
         return 1;
     else
         return 0;
-    
+
 }
 
 /*
@@ -64,12 +69,12 @@ int q2(char *datainicial, char *datafinal, int *qtdDias, int *qtdMeses, int *qtd
     *qtdDias = 10;
     *qtdAnos = 20;
     *qtdMeses = 30;
-    
+
     //printf("%s\n", datainicial);
     //printf("%s\n", datafinal);
-    
+
     return 1;
-    
+
 }
 
 
@@ -81,7 +86,7 @@ int q2(char *datainicial, char *datafinal, int *qtdDias, int *qtdMeses, int *qtd
     uma string texto, um caracter c e um inteiro que informa se é uma pesquisa Case Sensitive ou não. Se isCaseSensitive = 1, a pesquisa deve considerar diferências entre maiúsculos e minúsculos.
         Se isCaseSensitive != 1, a pesquisa não deve  considerar diferências entre maiúsculos e minúsculos.
  @saida
-    Um número n >= 0. 
+    Um número n >= 0.
  */
 
 int q3(char *texto, char c, int isCaseSensitive){
@@ -116,13 +121,12 @@ int q3(char *texto, char c, int isCaseSensitive){
         posicoes[1] = 16;
         Observe que o índice da posição no texto deve começar ser contado a partir de 1.
         O retorno da função, n, nesse caso seria 1;
-
  */
 int q4(char *strTexto, char *strBusca, int posicoes[30]){
     int qtdOcorrencias = -1;
-    
+
     return qtdOcorrencias;
-    
+
 }
 
 /*
@@ -151,6 +155,69 @@ int q5(int num){
  */
 
 int q6(int numerobase, int numerobusca){
-    int qtdOcorrencias;
+    int qtdOcorrencias=0,TAM=0,TamBusca=0,achou=0;
+    int j,k;
+    TAM = QtdDigitosBase(numerobase);
+    TamBusca = QtdDigitosBusca(numerobusca);
+    int vetorBase[TAM];
+    int vetorBusca[TamBusca];
+    for(int i=0;i<TAM;i++){  //PREENCHENDO VETOR DA BASE COM CADA DIGITO DO NUMERO DA BASE
+        vetorBase[i] = numerobase/x;
+        numerobase = numerobase%x;
+        x = x/10;
+    }
+    for(int i=0;i<TamBusca;i++){  //PREENCHENDO VETOR DA BUSCA COM CADA DIGITO DO NUMERO DA BUSCA
+        vetorBusca[i] = numerobusca/y;
+        numerobusca = numerobusca%y;
+        y = y/10;
+    }
+    //printf("Tamanho vetor base: %d\n Tamanho vetor busca: %d\n",TAM,TamBusca);
+    for(int i=0;i<TAM;i++){
+        j=i;
+        k=0;
+        //printf("Valor de i percorrendo o vetor: %d\n",i);
+        while(j<=TAM){
+            if(vetorBase[j]==vetorBusca[k]){
+                achou++;
+                //printf("Vetor base %d\n Vetor Busca %d\n",vetorBase[j],vetorBusca[k]);
+                j++;
+            }
+            else
+                break;
+
+
+
+            k++;
+        }
+        if(achou==TamBusca)
+            qtdOcorrencias++;
+            achou=0;
+    }
+    //printf("Quantidade de ocorrencia: %d\n",qtdOcorrencias);
+
     return qtdOcorrencias;
+}
+int QtdDigitosBase(int numerobase){
+        int i=1;
+        int qtd=0;
+        while(i<=numerobase){
+            i*=10;
+            qtd++;
+        }
+        x=i/10;
+
+        return qtd;
+
+}
+int QtdDigitosBusca(int numerobusca){
+        int i=1;
+        int qtd=0;
+        while(i<=numerobusca){
+            i*=10;
+            qtd++;
+        }
+        y=i/10;
+
+        return qtd;
+
 }
