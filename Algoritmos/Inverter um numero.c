38b5bf829e478e9da75b46471a519e67ef730a66
DaniloD30 <danilolive30@hotmail.com>
2017-07-21 01:48:41 -0300

    Update and rename Questão 5.c to Inverter um numero.c

--- Algoritmos/Inverter um numero.c
+++ Algoritmos/Inverter um numero.c
@@ -2,26 +2,27 @@
 #include <stdlib.h>
 
 int QtdDigitos(int num);
-
+int x;
 int q5(int num){
-    int TAM;
+    int TAM=0, mult=1, n=0;
+    TAM = QtdDigitos(num);
     int vetor[TAM];
-    int mult=1;
-    int n=0;
+    mult=x;
     for(int i=0;i<TAM;i++){  //PREENCHENDO VETOR DA BASE COM CADA DIGITO DO NUMERO DA BASE
         vetor[i] = num/x;
-        numerobase = num%x;
+        num = num%x;
         x = x/10;
     }
     for(int i=TAM-1;i>=0;i--){
         n+=vetor[i]*mult;
-        mult*=10;
+        mult=mult/10;
     }
+    num=n;
     return num;
 }
  int QtdDigitos(int num){
         int i=1;
-        int qtd=0;  
+        int qtd=0;
         while(i<=num){
             i*=10;
             qtd++;
@@ -29,10 +30,13 @@ int q5(int num){
         x=i/10;
 
         return qtd;
-
 }
-int main()
-{
-    printf("Hello world!\n");
+int main(){
+    int num,invertido=0;
+    printf("Digite um numero para ser invertido: ");
+    scanf("%d",&num);
+    invertido =  q5(num);
+    printf("Numero Invertido: %d\n",invertido);
+
     return 0;
 }
