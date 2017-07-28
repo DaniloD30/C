c3e5f7f483853066941083089e1ff5c4ec756670
Danilo Dias <danilolive30@hotmail.com>
2017-07-27 21:21:59 -0300

    Update Validação de data com entrada em String.c

--- Algoritmos/Validação de data com entrada em String.c
+++ Algoritmos/Validação de data com entrada em String.c
@@ -2,26 +2,95 @@
 #include <stdlib.h>
 #include<string.h>
 
-int main(){
-    char data[11];
+int q1(char *data){
+    int datavalida = 1;
     char dia[2],mes[2],ano[4];
-    int i,j,k,d=0,a=0,m=0,y=0,datavalida=0;
-    gets(data);
+    int i,j,k,d=0,a=0,m=0,y=0,v=0;
+
     for(i=0;data[i]!='/';i++){
         dia[i] = data[i];
-          printf("Dia: %c \n",dia[i]);
+          //printf("Dia: %c \n",dia[i]);
+    }
+    dia[i] = '\0';
+    for(j = i+1;data[j]!='/';j++){
+        mes[y] = data[j];
+          //printf("Mes: %c \n",mes[y]);
+        y++;
+    }
+    mes[y] = '\0';
+    y=0;
+    for(k = j+1;data[k]!='\0';k++){
+        ano[y] = data[k];
+        //printf("y: %d\n",y);
+        //printf("Ano: %c \n",ano[y]);
+        y++;
+    }
+    ano[y] = '\0';
+
+    if(strlen(ano)!=2 && strlen(ano)!= 4)
+        return 0;
+    if(strlen(dia)!=1 && strlen(dia)!= 2)
+        return 0;
+    if(strlen(mes)!=1 && strlen(mes)!= 2)
+        return 0;
+
+    d = atoi(dia);
+    m = atoi(mes);
+    a = atoi(ano);
+
+    if(d<=31 && d>0)
+        if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
+             datavalida = 1;
+             v = 1;
+        }
+    if(d<=30 && d>0)
+        if(m==4||m==6||m==9||m==11){
+            datavalida = 1;
+            v=1;
+        }
+    if(d==29 && m==2)
+        if((a%4==0 && a%100!=0 )|| (a%400==0 && a%100!=0)){
+            datavalida = 1;
+            v=1;
+        }
+    if(d<29 && m==2){
+        datavalida =1;
+        v=1;
+    }
+    if(v==0)
+        datavalida = 0;
+
+
+    if (datavalida)
+        return 1;
+    else
+        return 0;
+
+}
+int main(){
+    char data[11];
+    //char dia[2],mes[2],ano[4];
+    //int i,j,k;//d=0,a=0,m=0,y=0,//datavalida=0;
+    int valida =0;
+    gets(data);
+    valida = q1(data);
+    printf("%d\n",valida);
+    /*
+    for(i=0;data[i]!='/';i++){
+        dia[i] = data[i];
+          //printf("Dia: %c \n",dia[i]);
 
     }
     for(j = i+1;data[j]!='/';j++){
         mes[y] = data[j];
-          printf("Mes: %c \n",mes[y]);
+          //printf("Mes: %c \n",mes[y]);
         y++;
     }
     y=0;
     for(k = j+1;data[k]!='\0';k++){
         ano[y] = data[k];
         //printf("y: %d\n",y);
-        printf("Ano: %c \n",ano[y]);
+        //printf("Ano: %c \n",ano[y]);
         y++;
     }
     d = atoi(dia);
@@ -45,8 +114,8 @@ int main(){
     else
         return 0;
 
+*/
 
-
-    printf("Dia: %d  Mes: %d  Ano: %d\n",d,m,a);
+   // printf("Dia: %d  Mes: %d  Ano: %d\n",d,m,a);
     return 0;
 }
