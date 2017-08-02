4bac2fe13983bdc1d1d22d394b4506859d05e10f
Danilo Dias <danilolive30@hotmail.com>
2017-08-02 01:32:14 -0300

    Update Trabalho Laboratorio.c

--- Algoritmos/Trabalho Laboratorio.c
+++ Algoritmos/Trabalho Laboratorio.c
@@ -50,13 +50,13 @@ int q1(char *data){
         dia[i] = data[i];
           //printf("Dia: %c \n",dia[i]);
     }
-    dia[i] = '\0';
+    //dia[i] = '\0';
     for(j = i+1;data[j]!='/';j++){
         mes[y] = data[j];
           //printf("Mes: %c \n",mes[y]);
         y++;
     }
-    mes[y] = '\0';
+    //mes[y] = '\0';
     y=0;
     for(k = j+1;data[k]!='\0';k++){
         ano[y] = data[k];
@@ -64,47 +64,35 @@ int q1(char *data){
         //printf("Ano: %c \n",ano[y]);
         y++;
     }
-    ano[y] = '\0';
-
+    //ano[y] = '\0';
+    /*
     if(strlen(ano)!=2 && strlen(ano)!= 4)
         return 0;
     if(strlen(dia)!=1 && strlen(dia)!= 2)
         return 0;
     if(strlen(mes)!=1 && strlen(mes)!= 2)
         return 0;
+    */
 
     d = atoi(dia);
     m = atoi(mes);
     a = atoi(ano);
 
-    if(d<=31 && d>0)
-        if(m==1||m==3||m==5||m==7||m==8||m==10||m==12){
-             datavalida = 1;
-             v = 1;
-        }
-    if(d<=30 && d>0)
-        if(m==4||m==6||m==9||m==11){
-            datavalida = 1;
-            v=1;
-        }
-    if(d==29 && m==2)
-        if((a%4==0 && a%100!=0 )|| (a%400==0 && a%100!=0)){
-            datavalida = 1;
-            v=1;
-        }
-    if(d<29 && m==2){
-        datavalida =1;
-        v=1;
+    if((d > 0 && d <= 31) && (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) && a > 0){
+		return 1;
     }
-    if(v==0)
-        datavalida = 0;
-
-
-    if (datavalida)
+    else if((d > 0 && d <= 30) && (m == 4 || m == 6 || m == 9 || m == 11) && a > 0){
+		return 1;
+    }
+    else if ((d > 0 && d <= 29) && m == 2 && (a % 4 == 0 && (a % 400 == 0 || a % 100 != 0))){
         return 1;
-    else
+    }
+    else if((d>0 && d<=28) && m == 2 && a > 0 ){
+        return 1;
+    }
+    else{
         return 0;
-
+    }
 }
 
 /*
