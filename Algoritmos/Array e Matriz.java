66f273cd832d4af25af6a2df7370532a83ae5f1d
Danilo Dias <danilolive30@hotmail.com>
2017-12-12 22:46:10 -0200

    Create Array e Matriz.java

---
+++ Algoritmos/Array e Matriz.java
@@ -0,0 +1,25 @@
+_______CLASSE ___________________
+
+
+private int[][]valores = new int[5][];
+
+public void run2(){
+  for(int i = 0; i < valores.lenght; i++){
+          valores[i] = new int[i+1];
+  }
+}
+
+
+___ ARRAY DE TIPOS CLASSE _________
+
+
+private Gene[] genes = new Gene[10];
+
+public void run2(){
+  for(int i = 0; i < gene.lenght; i++){
+          this.genes[i] = new Genes(i);
+          System.out.println(this.genes[i].getValor());
+  }
+  
+}
+
