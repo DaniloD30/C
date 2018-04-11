34f1a23d42301ec4e53ff4c9028072390afbe5fb
Danilo Dias <danilolive30@hotmail.com>
2018-04-10 23:26:11 -0300

    Create AplicacoesPrimarias.java

---
+++ Java/TrabalhoPOO/Classes/AplicacoesPrimarias.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class AplicacoesPrimarias extends Aplicacoes {
+
+	public AplicacoesPrimarias(String codigo, String descricao, float estimativaDeRisco, float estimativaDeRendimento,
+			float fatia, float valorMinimo) {
+		super(codigo, descricao, estimativaDeRisco, estimativaDeRendimento, fatia, valorMinimo);
+		// TODO Auto-generated constructor stub
+	}
+
+}
